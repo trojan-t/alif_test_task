ab863e568ab7ae13fe170876e4d489370bee1f31
Muhammadkhon <muhammadkhonabdulloev@gmail.com>
2021-01-10 20:05:05 +0500

    EgyptianFraction is implemented

---
+++ pkg/egyptianFraction.go
@@ -0,0 +1,24 @@
+package pkg
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func EgyptianFraction(top, bottom float64) string {
+	bottomList := []float64{}
+	for top != 0 {
+		x := math.Ceil(bottom / top)
+		bottomList = append(bottomList, x)
+		top = x*top - bottom
+		bottom = bottom * x
+	}
+	var sb strings.Builder
+	for _, currentBottom := range bottomList {
+		sb.WriteString("1 / ")
+		sb.WriteString(strconv.Itoa(int(currentBottom)))
+		sb.WriteString(", ")
+	}
+	return sb.String()
+}
